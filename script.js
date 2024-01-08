e486ce0f3db8b374e5dea7c07f7594538fd00f64
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-07 19:01:03 -0600

    feat: add button to reset game

--- script.js
+++ script.js
@@ -18,30 +18,37 @@ const Gameboard = (function () {
     const cols = 3;
     const board = [];
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < cols; j++) {
-            board[i].push(Cell());
+    const initBoard = () => {
+        board.splice(0, board.length);
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < cols; j++) {
+                board[i].push(Cell());
+            }
         }
-    }
+    };
 
+    
     const getBoard = () => board;
-
+    
     const printBoard = () => {
         const boardValues = board.map((row) =>
-            row.map((cell) => cell.getValue())
+        row.map((cell) => cell.getValue())
         );
         console.table(boardValues);
     };
-
+    
     const placeMarker = (row, column, marker) => {
         board[row][column].addMarker(marker);
     };
-
+    
+    initBoard();
+    
     return {
         getBoard,
         printBoard,
         placeMarker,
+        initBoard,
     };
 })();
 
@@ -49,27 +56,26 @@ const GameController = (function (
     playerOneName = "Player One",
     playerTwoName = "Player Two"
 ) {
-
     const players = [
         { name: playerOneName, marker: "X" },
         { name: playerTwoName, marker: "O" },
     ];
 
-
-    
     let currentPlayer = players[0];
-    
+
     const switchCurrentPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     };
-    
+
     const getCurrentPlayerTurn = () => currentPlayer;
-    
+
     const changePlayerNames = (playerOneName, playerTwoName) => {
+        if (playerOneName === "") playerOneName = "X";
+        if (playerTwoName === "") playerTwoName = "O";
         players[0].name = playerOneName;
         players[1].name = playerTwoName;
-    }
-    
+    };
+
     let rowX = new Array(3).fill(0);
     let colX = new Array(3).fill(0);
     let diagX = 0;
@@ -109,15 +115,36 @@ const GameController = (function (
     };
 
     let moveCount = 0;
+
     const checkTie = () => {
         ++moveCount;
         if (moveCount === 9) {
             return true;
         } else return false;
-    }
+    };
 
     let gameWon = false;
     let gameTied = false;
+
+    const resetWinCheck = () => {
+        rowX = new Array(3).fill(0);
+        colX = new Array(3).fill(0);
+        diagX = 0;
+        antiDiagX = 0;
+
+        rowO = new Array(3).fill(0);
+        colO = new Array(3).fill(0);
+        diagO = 0;
+        antiDiagO = 0;
+
+        moveCount = 0;
+    };
+
+    const resetGameStatus = () => {
+        gameWon = false;
+        gameTied = false;
+        resetWinCheck();
+    };
 
     const getGameWon = () => gameWon;
     const getGameTied = () => gameTied;
@@ -140,18 +167,18 @@ const GameController = (function (
         getCurrentPlayerTurn,
         getGameWon,
         getGameTied,
-        changePlayerNames
+        resetGameStatus,
+        changePlayerNames,
     };
 })();
 
-// make this work
 const displayController = (function () {
     const game = GameController;
 
     const board = Gameboard.getBoard();
 
-    const infoDisplay = document.querySelector('.game-info');
-    infoDisplay.textContent = ``
+    const infoDisplay = document.querySelector(".game-info");
+    infoDisplay.textContent = ``;
     const boardDisplay = document.querySelector(".board");
     const updateCellDisplay = () => {
         boardDisplay.textContent = "";
@@ -167,42 +194,67 @@ const displayController = (function () {
         }
     };
 
-
     function clickHandlerBoard(event) {
         const row = event.target.dataset.row;
         const col = event.target.dataset.column;
         game.playRound(row, col);
         updateCellDisplay();
         if (game.getGameWon()) {
-            infoDisplay.textContent = `${game.getCurrentPlayerTurn().name} won!`
+            infoDisplay.textContent = `${
+                game.getCurrentPlayerTurn().name
+            } won!`;
         } else if (game.getGameTied()) {
-            infoDisplay.textContent = `It's a tie!`
+            infoDisplay.textContent = `It's a tie!`;
         } else {
-            infoDisplay.textContent = `${game.getCurrentPlayerTurn().name}'s turn`
+            infoDisplay.textContent = `${
+                game.getCurrentPlayerTurn().name
+            }'s turn`;
         }
     }
 
-    boardDisplay.addEventListener('click', clickHandlerBoard);
-
     const getPlayerInputs = () => {
-        const playerOneInput = document.querySelector('#player-one').value;
-        const playerTwoInput = document.querySelector('#player-two').value;
+        const playerOneInput = document.querySelector("#player-one").value;
+        const playerTwoInput = document.querySelector("#player-two").value;
         const inputs = [playerOneInput, playerTwoInput];
         return inputs;
-    }
+    };
 
-    const startButton = document.querySelector('.start');
+    const controlDisplay = document.querySelector(".controls");
+    const startButton = document.createElement("button");
+    startButton.className = "start";
+    startButton.textContent = "Start game";
+    const resetButton = document.createElement("button");
+    resetButton.className = "reset";
+    resetButton.textContent = "New game";
 
-    function clickHandlerButton() {
+    const displayButton = (button) => {
+        controlDisplay.appendChild(button);
+    };
+
+    function clickHandlerStartButton() {
         const [playerOneName, playerTwoName] = getPlayerInputs();
         GameController.changePlayerNames(playerOneName, playerTwoName);
-        displayController.updateCellDisplay();
+        updateCellDisplay();
+        controlDisplay.removeChild(startButton);
+        displayButton(resetButton);
     }
 
-    startButton.addEventListener('click', clickHandlerButton)
+    function clickHandlerResetButton() {
+        Gameboard.initBoard();
+        GameController.resetGameStatus();
+        const [playerOneName, playerTwoName] = getPlayerInputs();
+        GameController.changePlayerNames(playerOneName, playerTwoName);
+        updateCellDisplay();
+        infoDisplay.textContent = "";
+    }
+
+    boardDisplay.addEventListener("click", clickHandlerBoard);
+    startButton.addEventListener("click", clickHandlerStartButton);
+    resetButton.addEventListener("click", clickHandlerResetButton);
+    displayButton(startButton);
 
     return {
         updateCellDisplay,
-        getPlayerInputs
+        getPlayerInputs,
     };
 })();
